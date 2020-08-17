ac8132a1f481d4c85bc3e3e71a93d67b67cbde40
Sunil Singh <singh.sunil7725@gmail.com>
2020-08-17 11:44:53 +0530

    screen

--- app/src/main/java/com/example/oops/activity/Profile.java
+++ app/src/main/java/com/example/oops/activity/Profile.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.AppCompatTextView;
 
 import com.example.oops.R;
@@ -12,22 +11,18 @@ import com.example.oops.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class Support_Help extends AppCompatActivity {
+public class Profile extends AppCompatActivity {
     @BindView(R.id.txtHeading)
     AppCompatTextView txtHeading;
-    @BindView(R.id.editTextMessage)
-    AppCompatEditText editTextMessage;
-    String seditTextMessage;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.support_help);
+        setContentView(R.layout.profile);
         ButterKnife.bind(this);
         initView();
     }
 
     private void initView() {
-        txtHeading.setText(getString(R.string.support_help));
+        txtHeading.setText(getString(R.string.profile));
     }
 }
---
+++ app/src/main/java/com/example/oops/activity/UserRegistration.java
@@ -0,0 +1,17 @@
+package com.example.oops.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.oops.R;
+
+public class UserRegistration extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_registration);
+
+    }
+}
