f9ba5e27fb80de033e53817781e91d57f659ac8c
Sunil Singh <singh.sunil7725@gmail.com>
2020-08-18 16:40:38 +0530

    SCREEN

--- app/src/main/java/com/example/oops/activity/SearchHere.java
+++ app/src/main/java/com/example/oops/activity/SearchHere.java
@@ -1,4 +1,16 @@
 package com.example.oops.activity;
 
-public class SearchHere {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.oops.R;
+
+public class SearchHere extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_here);
+    }
 }
--- app/src/main/java/com/example/oops/activity/VideoPlay.java
+++ app/src/main/java/com/example/oops/activity/VideoPlay.java
@@ -1,6 +1,45 @@
 package com.example.oops.activity;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.oops.R;
+
+import butterknife.BindView;
 
 public class VideoPlay extends AppCompatActivity {
+    @BindView(R.id.txtVideoHeading)
+    AppCompatTextView txtVideoHeading;
+    String stxtVideoHeading;
+
+    @BindView(R.id.txtContentType)
+    AppCompatTextView txtContentType;
+    String stxtContentType;
+    @BindView(R.id.txtRate)
+    AppCompatTextView txtRate;
+    String stxtRate;
+    @BindView(R.id.txtVideoType)
+    AppCompatTextView txtVideoType;
+    String stxtVideoType;
+    @BindView(R.id.txtSoryLine)
+    AppCompatTextView txtSoryLine;
+    String stxtSoryLine;
+    @BindView(R.id.txtCastName)
+    AppCompatTextView txtCastName;
+    String stxtCastName;
+    @BindView(R.id.txtDirectorName)
+    AppCompatTextView txtDirectorName;
+    String stxtDirectorName;
+    @BindView(R.id.recylerview)
+    RecyclerView recylerview;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_player);
+    }
 }
