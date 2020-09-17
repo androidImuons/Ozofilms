a7a49718ea513c7da5d21eee6a60aa16f4d6fa3a
Sunil Singh <singh.sunil7725@gmail.com>
2020-09-17 10:15:56 +0530

    Offline Streaming Add

--- app/src/main/java/com/example/oops/activity/TestVideoDownload.java
+++ app/src/main/java/com/example/oops/activity/TestVideoDownload.java
@@ -2,7 +2,6 @@ package com.example.oops.activity;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,42 +11,23 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatImageView;
-import androidx.appcompat.widget.AppCompatTextView;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.oops.DataClass.CategoryListData;
-import com.example.oops.DataClass.MovieDeatilsData;
 import com.example.oops.Ooops;
 import com.example.oops.R;
-import com.example.oops.ResponseClass.CategoryResponse;
-import com.example.oops.ResponseClass.CommonResponse;
-import com.example.oops.ResponseClass.MovieDeatilsResponse;
-import com.example.oops.Utils.AppCommon;
 import com.example.oops.Utils.AppUtil;
 import com.example.oops.Utils.DemoDownloadService;
 import com.example.oops.Utils.DownloadTracker;
 import com.example.oops.Utils.TrackKey;
-import com.example.oops.Utils.ViewUtils;
-import com.example.oops.adapter.RelatedAdapter;
-import com.example.oops.data.database.AppDatabase;
-import com.example.oops.data.database.Subtitle;
-import com.example.oops.data.database.Video;
-import com.example.oops.data.model.VideoSource;
-import com.example.oops.retrofit.AppService;
-import com.example.oops.retrofit.ServiceGenerator;
-import com.facebook.drawee.view.SimpleDraweeView;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
@@ -69,23 +49,13 @@ import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultAllocator;
 import com.google.android.exoplayer2.util.Util;
-import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 import static com.google.android.exoplayer2.offline.Download.STATE_COMPLETED;
 import static com.google.android.exoplayer2.offline.Download.STATE_DOWNLOADING;
@@ -95,38 +65,9 @@ import static com.google.android.exoplayer2.offline.Download.STATE_REMOVING;
 import static com.google.android.exoplayer2.offline.Download.STATE_RESTARTING;
 import static com.google.android.exoplayer2.offline.Download.STATE_STOPPED;
 
-public class VideoPlay extends AppCompatActivity implements View.OnClickListener, DownloadTracker.Listener {
-    @BindView(R.id.txtVideoHeading)
-    AppCompatTextView txtVideoHeading;
-    String stxtVideoHeading;
+public class TestVideoDownload extends AppCompatActivity implements View.OnClickListener, DownloadTracker.Listener {
 
-    @BindView(R.id.txtContentType)
-    AppCompatTextView txtContentType;
-    @BindView(R.id.txtRate)
-    AppCompatTextView txtRate;
-    @BindView(R.id.txtVideoType)
-    AppCompatTextView txtVideoType;
-    @BindView(R.id.txtSoryLine)
-    AppCompatTextView txtSoryLine;
-    @BindView(R.id.txtCastName)
-    AppCompatTextView txtCastName;
-    @BindView(R.id.txtDirectorName)
-    AppCompatTextView txtDirectorName;
-    @BindView(R.id.recylerview)
-    RecyclerView recylerview;
-    @BindView(R.id.sdvImage)
-    SimpleDraweeView sdvImage;
-    @BindView(R.id.txtHeading)
-    AppCompatTextView  txtHeading;
-    @BindView(R.id.imgPlayVideo)
-    AppCompatImageView imgPlayVideo;
-    private AppDatabase database;
-    private List<Subtitle> subtitleList = new ArrayList<>();
-    ArrayList<CategoryListData> categoryList;
-    RelatedAdapter relatedAdapter;
-LinearLayout linearLayout;
-ImageView imgDownload;
-    private List<Video> videoUriList = new ArrayList<>();
+
     ProgressDialog pDialog;
     protected static final CookieManager DEFAULT_COOKIE_MANAGER;
 
@@ -195,27 +136,25 @@ ImageView imgDownload;
         return false;
     }
 
+    @SuppressLint("WrongViewCast")
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+
         dataSourceFactory = buildDataSourceFactory();
         if (CookieHandler.getDefault() != DEFAULT_COOKIE_MANAGER) {
             CookieHandler.setDefault(DEFAULT_COOKIE_MANAGER);
         }
-        setContentView(R.layout.video_player);
-        ButterKnife.bind(this);
-        setLayout();
-        initializeDb();
+//        hideStatusBar();
 
-        if(getIntent()!= null){
-            String movieId = getIntent().getStringExtra("moviesId");
-            String name = getIntent().getStringExtra("name");
-            txtVideoHeading.setText(name);
-            txtHeading.setVisibility(View.GONE);
+        setContentView(R.layout.testdownload);
 
-            callGetMoviesDetails(movieId);
-        }
-        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
         if (savedInstanceState != null) {
             trackSelectorParameters = savedInstanceState.getParcelable(KEY_TRACK_SELECTOR_PARAMETERS);
             startAutoPlay = savedInstanceState.getBoolean(KEY_AUTO_PLAY);
@@ -229,22 +168,26 @@ ImageView imgDownload;
         Ooops application = (Ooops) getApplication();
         downloadTracker = application.getDownloadTracker();
         downloadManager = application.getDownloadManager();
+        img = (ImageView) findViewById(R.id.img);
+
+//        img.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(TestVideoDownload.this,DownloadActivity.class));
+//            }
+//        });
 
         try {
             DownloadService.start(this, DemoDownloadService.class);
         } catch (IllegalStateException e) {
             DownloadService.startForeground(this, DemoDownloadService.class);
         }
-        handler = new Handler();
 
 
 
-        imgDownload = (ImageView) findViewById(R.id.imgDownload);
+
+        createView();
 //        prepareView();
-        imgDownload.setOnClickListener(VideoPlay.this);
-        videoDurationInSeconds = MediaPlayer.create(VideoPlay.this, Uri.parse(videoUrl)).getDuration();
-        videoDurationInSeconds = videoDurationInSeconds % 60 ;
-
 
 
         runnableCode = new Runnable() {
@@ -257,214 +200,23 @@ ImageView imgDownload;
 
         handler.post(runnableCode);
 
-
-    }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        database.clearAllTables();
-        database = null;
-
-    }
-    private void setLayout() {
-
-        categoryList = new ArrayList<>();
-        relatedAdapter = new RelatedAdapter(VideoPlay.this , categoryList );
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recylerview.setLayoutManager(mLayoutManager);
-        recylerview.setItemAnimator(new DefaultItemAnimator());
-        recylerview.setAdapter(relatedAdapter);
-
-
-        imgPlayVideo.setOnClickListener(view -> goToPlayerActivity(makeVideoSource(videoUriList, 0)));
-    }
-
-    private void initializeDb() {
-        database = AppDatabase.Companion.getDatabase(getApplicationContext());
-    }
-
-    @OnClick(R.id.imgBackPressed)
-    public  void setImgBackPressed(){
-        onBackPressed();
     }
 
 
-    private void callGetMoviesDetails(String movieId) {
 
-        if (AppCommon.getInstance(this).isConnectingToInternet(this)) {
-            Dialog dialog = ViewUtils.getProgressBar(VideoPlay.this);
-            AppCommon.getInstance(this).setNonTouchableFlags(this);
-            AppService apiService = ServiceGenerator.createService(AppService.class , AppCommon.getInstance(this).getToken());
-            Map<String , String> entityMap = new HashMap<>();
-            entityMap.put("id" , String.valueOf(AppCommon.getInstance(this).getId()));
-            entityMap.put("userId" , String.valueOf(AppCommon.getInstance(this).getUserId()));
-            entityMap.put("movieId" , String.valueOf(movieId));
-            Call call = apiService.MOVIE_DEATILS_RESPONSE_CALL(entityMap);
-            call.enqueue(new Callback() {
-                @Override
-                public void onResponse(Call call, Response response) {
-                    AppCommon.getInstance(VideoPlay.this).clearNonTouchableFlags(VideoPlay.this);
-                    dialog.dismiss();
-                    MovieDeatilsResponse authResponse = (MovieDeatilsResponse) response.body();
-                    if (authResponse != null) {
-                        Log.i("Response::", new Gson().toJson(authResponse));
-                        if (authResponse.getCode() == 200) {
-                            if(authResponse.getData() != null)
-                            setData(authResponse.getData());
-                        } else {
+    protected void createView() {
+        handler = new Handler();
 
-                            Toast.makeText(VideoPlay.this, authResponse.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        AppCommon.getInstance(VideoPlay.this).showDialog(VideoPlay.this, "Server Error");
-                    }
-                }
-
-                @Override
-                public void onFailure(Call call, Throwable t) {
-                    dialog.dismiss();
-                    AppCommon.getInstance(VideoPlay.this).clearNonTouchableFlags(VideoPlay.this);
-                    // loaderView.setVisibility(View.GONE);
-                    Toast.makeText(VideoPlay.this, "Server Error", Toast.LENGTH_SHORT).show();
-                }
-            });
+        llParentContainer = (LinearLayout) findViewById(R.id.ll_parent_container);
 
 
-        } else {
-            // no internet
-            Toast.makeText(this, "Please check your internet", Toast.LENGTH_SHORT).show();
-        }
-    }
-    private void callRelativeMovies(MovieDeatilsData data) {
 
-        if (AppCommon.getInstance(this).isConnectingToInternet(this)) {
-            Dialog dialog = ViewUtils.getProgressBar(VideoPlay.this);
-            AppCommon.getInstance(this).setNonTouchableFlags(this);
-            AppService apiService = ServiceGenerator.createService(AppService.class , AppCommon.getInstance(this).getToken());
-            Map<String , String> entityMap = new HashMap<>();
-            entityMap.put("id" , String.valueOf(AppCommon.getInstance(this).getId()));
-            entityMap.put("userId" , String.valueOf(AppCommon.getInstance(this).getUserId()));
-            entityMap.put("movieId" , String.valueOf(data.getMovieId()));
-            entityMap.put("category" , String.valueOf(data.getMovieCategory()));
-            Call call = apiService.GetRelativeMovies(entityMap);
-            call.enqueue(new Callback() {
-                @Override
-                public void onResponse(Call call, Response response) {
-                    AppCommon.getInstance(VideoPlay.this).clearNonTouchableFlags(VideoPlay.this);
-                    dialog.dismiss();
-                    CategoryResponse authResponse = (CategoryResponse) response.body();
-                    if (authResponse != null) {
-                        Log.i("Response::", new Gson().toJson(authResponse));
-                        if (authResponse.getCode() == 200) {
-                            if(authResponse.getData() != null)
-                                if(authResponse.getData().size()!= 0) {
-                                    categoryList = authResponse.getData();
-                                    relatedAdapter.upadate(authResponse.getData());
-                                }
-                        } else {
+        btnAbc = (Button) findViewById(R.id.btnAbc);
 
-                            Toast.makeText(VideoPlay.this, authResponse.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        AppCommon.getInstance(VideoPlay.this).showDialog(VideoPlay.this, "Server Error");
-                    }
-                }
+        btnAbc.setOnClickListener(this);
+        videoDurationInSeconds = MediaPlayer.create(TestVideoDownload.this, Uri.parse(videoUrl)).getDuration();
+        videoDurationInSeconds = videoDurationInSeconds % 60 ;
 
-                @Override
-                public void onFailure(Call call, Throwable t) {
-                    dialog.dismiss();
-                    AppCommon.getInstance(VideoPlay.this).clearNonTouchableFlags(VideoPlay.this);
-                    // loaderView.setVisibility(View.GONE);
-                    Toast.makeText(VideoPlay.this, "Server Error", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-
-        } else {
-            // no internet
-            Toast.makeText(this, "Please check your internet", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void setData(MovieDeatilsData data) {
-        callRelativeMovies(data);
-        if(data.getMovieLongDescription() != null)
-        txtSoryLine.setText(data.getMovieLongDescription());
-        else
-            txtSoryLine.setText("N/A");
-
-        if(data.getCast() != null)
-            txtCastName.setText(data.getCast());
-        else
-            txtCastName.setText("N/A");
-
-        if(data.getDirector() != null)
-            txtDirectorName.setText(data.getDirector());
-        else
-            txtDirectorName.setText("N/A");
-
-        if(data.getCategoryName() != null)
-            txtVideoType.setText(data.getCategoryName());
-        else
-            txtVideoType.setText("N/A");
-
-        sdvImage.setController(AppCommon.getInstance(this).getDraweeController(sdvImage , data.getBannerLink() , 1024));
-        makeListOfUri(data);
-    }
-
-
-    private void makeListOfUri(MovieDeatilsData data) {
-        videoUriList.add(new Video(data.getVideoLink() , Long.getLong("zero" , 1)));
-
-        /*videoUriList.add(new Video("https://5b44cf20b0388.streamlock.net:8443/vod/smil:bbb.smil/playlist.m3u8", Long.getLong("zero", 1)));
-
-        subtitleList.add(new Subtitle(2, "German", "https://durian.blender.org/wp-content/content/subtitles/sintel_en.srt"));
-        subtitleList.add(new Subtitle(2, "French", "https://durian.blender.org/wp-content/content/subtitles/sintel_fr.srt"));
-*/
-        if (database.videoDao().getAllUrls().size() == 0) {
-            database.videoDao().insertAllVideoUrl(videoUriList);
-            database.videoDao().insertAllSubtitleUrl(subtitleList);
-        }
-
-    }
-
-    private VideoSource makeVideoSource(List<Video> videos, int index) {
-        setVideosWatchLength();
-        List<VideoSource.SingleVideo> singleVideos = new ArrayList<>();
-        for (int i = 0; i < videos.size(); i++) {
-
-            singleVideos.add(i, new VideoSource.SingleVideo(
-                    videos.get(i).getVideoUrl(),
-                    database.videoDao().getAllSubtitles(i + 1),
-                    videos.get(i).getWatchedLength())
-            );
-
-        }
-        return new VideoSource(singleVideos, index);
-    }
-
-    private List<Video> setVideosWatchLength() {
-        List<Video> videosInDb = database.videoDao().getVideos();
-        for (int i = 0; i < videosInDb.size(); i++) {
-            //videoUriList.get(i).setWatchedLength(videosInDb.get(i).getWatchedLength());
-            videoUriList.get(i).setWatchedLength(videosInDb.get(i).getWatchedLength());
-        }
-        return videoUriList;
-    }
-
-
-    //start player for result due to future features
-    public void goToPlayerActivity(VideoSource videoSource) {
-        int REQUEST_CODE = 1000;
-        Intent intent = new Intent(getApplicationContext(), PlayerActivity.class);
-        intent.putExtra("videoSource", videoSource);
-        startActivityForResult(intent, REQUEST_CODE);
-    }
-
-    public void clickRelativeMovies(int adapterPosition) {
-        startActivity(new Intent(this , VideoPlay.class)
-                .putExtra("moviesId" , categoryList.get(adapterPosition).getMovieId())
-                .putExtra("name" , categoryList.get(adapterPosition).getMovieName()));
     }
 
 
@@ -522,7 +274,11 @@ ImageView imgDownload;
 
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
 
+    }
 
 
 
@@ -538,7 +294,7 @@ ImageView imgDownload;
 
 
 
-            case R.id.imgDownload:
+            case R.id.btnAbc:
                 fetchDownloadOptions();
 
 
@@ -559,7 +315,7 @@ ImageView imgDownload;
         trackKeys.clear();
 
         if (pDialog == null || !pDialog.isShowing()) {
-            pDialog = new ProgressDialog(VideoPlay.this);
+            pDialog = new ProgressDialog(TestVideoDownload.this);
             pDialog.setTitle(null);
             pDialog.setCancelable(false);
             pDialog.setMessage("Preparing Download Options...");
@@ -567,7 +323,7 @@ ImageView imgDownload;
         }
 
 
-        DownloadHelper downloadHelper = DownloadHelper.forHls(VideoPlay.this, Uri.parse(videoUrl), dataSourceFactory, new DefaultRenderersFactory(VideoPlay.this));
+        DownloadHelper downloadHelper = DownloadHelper.forHls(TestVideoDownload.this, Uri.parse(videoUrl), dataSourceFactory, new DefaultRenderersFactory(TestVideoDownload.this));
 
 
         downloadHelper.prepare(new DownloadHelper.Callback() {
@@ -612,7 +368,7 @@ ImageView imgDownload;
             return;
         }
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(VideoPlay.this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(TestVideoDownload.this);
         builder.setTitle("Select Download Format");
         int checkedItem = 1;
 
@@ -628,7 +384,7 @@ ImageView imgDownload;
 
         // Initialize a new array adapter instance
         ArrayAdapter arrayAdapter = new ArrayAdapter<String>(
-                VideoPlay.this, // Context
+                TestVideoDownload.this, // Context
                 android.R.layout.simple_list_item_single_choice, // Layout
                 optionsToDownload // List
         );
@@ -890,7 +646,6 @@ ImageView imgDownload;
         startWindow = C.INDEX_UNSET;
         startPosition = C.TIME_UNSET;
     }
-
 
 
 
--- app/src/main/java/com/example/oops/fragment/DownloadVideo.java
+++ app/src/main/java/com/example/oops/fragment/DownloadVideo.java
@@ -1,16 +1,32 @@
 package com.example.oops.fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SimpleItemAnimator;
 
+import com.example.oops.Ooops;
 import com.example.oops.R;
+import com.example.oops.Utils.AppUtil;
+import com.example.oops.adapter.DownloadedVideoAdapter;
+import com.example.oops.model.VideoModel;
+import com.google.android.exoplayer2.offline.Download;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,21 +39,147 @@ public class DownloadVideo extends Fragment {
     AppCompatImageView imgBackPressed;
     @BindView(R.id.recylerview)
     RecyclerView recylerview;
-
+    private List<Download> downloadedVideoList;
+    private DownloadedVideoAdapter downloadedVideoAdapter;
+    private Runnable runnableCode;
+    private Handler handler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.downloadvideofragment, container, false);
-        ButterKnife.bind(this,view);
+        ButterKnife.bind(this, view);
         txtHeading.setText(getString(R.string.downloads));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recylerview.setLayoutManager(linearLayoutManager);
+        ((SimpleItemAnimator) recylerview.getItemAnimator()).setSupportsChangeAnimations(false);
+        loadVideos();
 
+        handler = new Handler();
+        runnableCode = new Runnable() {
+            @Override
+            public void run() {
+                List<Download> exoVideoList = new ArrayList<>();
+                for (Map.Entry<Uri, Download> entry : Ooops.getInstance().getDownloadTracker().downloads.entrySet()) {
+                    Uri keyUri = entry.getKey();
+                    Download download = entry.getValue();
+                    exoVideoList.add(download);
+                }
+                downloadedVideoAdapter.onNewData(exoVideoList);
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(runnableCode);
         return view;
 
     }
-    @OnClick(R.id.imgBackPressed)
-    public  void setImgBackPressed(){
 
+    @OnClick(R.id.imgBackPressed)
+    public void setImgBackPressed() {
+
+    }
+
+
+    private void loadVideos() {
+        downloadedVideoList = new ArrayList<>();
+
+        for (Map.Entry<Uri, Download> entry : Ooops.getInstance().getDownloadTracker().downloads.entrySet()) {
+            Uri keyUri = entry.getKey();
+            Download download = entry.getValue();
+            downloadedVideoList.add(download);
+        }
+
+
+        downloadedVideoAdapter = new DownloadedVideoAdapter(getContext(),DownloadVideo.this);
+        recylerview.setAdapter(downloadedVideoAdapter);
+        downloadedVideoAdapter.addItems(downloadedVideoList);
+
+
+    }
+
+    public void openBottomSheet(Download download){
+
+        VideoModel videoModel = AppUtil.getVideoDetail(download.request.id);
+
+        String statusTitle = videoModel.getVideoName();
+
+        View dialogView = getLayoutInflater().inflate(R.layout.fragment_bottom_sheet_dialog, null);
+        BottomSheetDialog dialog = new BottomSheetDialog(getActivity());
+        dialog.setContentView(dialogView);
+
+        TextView tvVideoTitle =  dialog.findViewById(R.id.tv_video_title);
+        LinearLayout llDownloadStart = dialog.findViewById(R.id.ll_start_download);
+        LinearLayout llDownloadResume = dialog.findViewById(R.id.ll_resume_download);
+        LinearLayout llDownloadPause = dialog.findViewById(R.id.ll_pause_download);
+        LinearLayout llDownloadDelete = dialog.findViewById(R.id.ll_delete_download);
+
+        llDownloadStart.setVisibility(View.GONE);
+
+
+        if(download.state == Download.STATE_DOWNLOADING){
+            llDownloadPause.setVisibility(View.VISIBLE);
+            llDownloadResume.setVisibility(View.GONE);
+
+        }else if(download.state == Download.STATE_STOPPED){
+            llDownloadPause.setVisibility(View.GONE);
+            llDownloadResume.setVisibility(View.VISIBLE);
+
+        } else if(download.state == Download.STATE_QUEUED){
+            llDownloadStart.setVisibility(View.VISIBLE);
+            llDownloadPause.setVisibility(View.GONE);
+            llDownloadResume.setVisibility(View.GONE);
+        }else {
+            llDownloadStart.setVisibility(View.GONE);
+            llDownloadPause.setVisibility(View.GONE);
+            llDownloadResume.setVisibility(View.GONE);
+        }
+
+        tvVideoTitle.setText(statusTitle);
+        llDownloadStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ooops.getInstance().getDownloadManager().addDownload(download.request);
+                dialog.dismiss();
+            }
+        });
+        llDownloadResume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ooops.getInstance().getDownloadManager().addDownload(download.request, Download.STOP_REASON_NONE);
+
+                dialog.dismiss();
+            }
+        });
+
+        llDownloadPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ooops.getInstance().getDownloadManager().addDownload(download.request, Download.STATE_STOPPED);
+                dialog.dismiss();
+            }
+        });
+
+        llDownloadDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ooops.getInstance().getDownloadManager().removeDownload(download.request.id);
+
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+
+
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        handler.removeCallbacks(runnableCode);
     }
 }
